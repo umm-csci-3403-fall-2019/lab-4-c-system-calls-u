03c0a9c687c843253806c102d083390944b9b6b1
Trent Russell Merkins <merki002@umm9020-h2ytx12.morris.umn.edu>
2019-10-14 17:04:21 -0500

    I added comments to the summarize_tree.sh

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+#sets the argument to a directory variable
 dir=$1
 
+#This uses find to find all the subdirectories. wc -l counts the lines
+#this is equal to the number of directories.
 numdir=$(find $dir -type d | wc -l)
 echo "There were" $numdir "directories."
 
+#This does a similar thing, but searches for file type.
 numfiles=$(find $dir -type f | wc -l)
 echo "There were" $numfiles "regular files."
 
